3d308cd0dc039e33e7b755c118203212d32a5ab6
DangVuMinhHieu <protopro03@gmail.com>
2018-11-05 21:07:36 +0700

    Finish TODO 1

---
+++ vietai-assignment2/activation_np.py
@@ -0,0 +1,85 @@
+"""activation_np.py
+This file provides activation functions for the NN 
+Author: Phuong Hoang
+"""
+
+import numpy as np
+import tensorflow as tf
+
+def sigmoid(x):
+    """sigmoid
+    TODO: 
+    Sigmoid function. Output = 1 / (1 + exp(-x)).
+    :param x: input
+    """
+    #[TODO 1.1]
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_grad(a):
+    """sigmoid_grad
+    TODO:
+    Compute gradient of sigmoid with respect to input. g'(x) = g(x)*(1-g(x))
+    :param a: output of the sigmoid function
+    """
+    #[TODO 1.1]
+    return a * (1 - a)
+
+
+def reLU(x):
+    """reLU
+    TODO:
+    Rectified linear unit function. Output = max(0,x).
+    :param x: input
+    """
+    #[TODO 1.1]
+    return max(0, x)
+
+
+def reLU_grad(a):
+    """reLU_grad
+    TODO:
+    Compute gradient of ReLU with respect to input
+    :param x: output of ReLU
+    """
+    #[TODO 1.1]
+    grad = a > 0
+    return grad
+
+
+def tanh(x):
+    """tanh
+    TODO:
+    Tanh function.
+    :param x: input
+    """
+    #[TODO 1.1]
+    return 2 / (1 + np.exp(-2 * x)) - 1
+
+
+def tanh_grad(a):
+    """tanh_grad
+    TODO:
+    Compute gradient for tanh w.r.t input
+    :param a: output of tanh
+    """
+    #[TODO 1.1]
+    return 1 - a ** 2
+
+
+def softmax(x):
+    """softmax
+    TODO:
+    Softmax function.
+    :param x: input
+    """
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def softmax_minus_max(x):
+    """softmax_minus_max
+    TODO:
+    Stable softmax function.
+    :param x: input
+    """
+    return np.exp(x - np.max(x)) / np.sum(np.exp(x), axis=0)
